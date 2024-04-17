a84f54d07793534bbe0e49f34107ed5bfc712d81
Bebe Jane <bjornthief@gmail.com>
2024-04-17 15:47:14 +0200

    test2

--- app/@modal/(.)projects/[project]/page.tsx
+++ app/@modal/(.)projects/[project]/page.tsx
@@ -1,15 +1,14 @@
 import s from './page.module.scss';
 import ProjectPage from '@app/projects/[project]/page';
 import { Suspense } from 'react';
-import { Modal } from '@components/Modal';
 
 export default function Page({ params }: { params: { project: string } }) {
 
   return (
     <div className={s.modal}>
-      <Suspense fallback={<div className={s.loading}></div>}>
-        <ProjectPage params={params} modal={true} />
-      </Suspense>
+
+      <ProjectPage params={params} modal={true} />
+
     </div>
   )
 }
