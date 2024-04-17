94e09e10ccf1138673cf09829b3db83aae03825f
Bebe Jane <bjornthief@gmail.com>
2024-04-17 17:07:55 +0200

    jepp

--- app/components/Overview.tsx
+++ app/components/Overview.tsx
@@ -5,6 +5,7 @@ import cn from 'classnames'
 import { Block } from 'next-dato-utils/components';
 import * as Blocks from '@components/blocks'; import { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
+import { useScrollInfo } from 'next-dato-utils/hooks';
 
 export type Props = {
   overview: OverviewQuery['overview'];
@@ -14,9 +15,16 @@ export default function Overview({ overview }: Props) {
 
   const isHome = usePathname() === '/';
   const [title, setTitle] = useState<string | null>(null)
+  const { scrolledPosition, viewportHeight } = useScrollInfo()
+
+  useEffect(() => {
+    if (scrolledPosition < (viewportHeight / 2))
+      setTitle(null)
+
+  }, [scrolledPosition, viewportHeight])
 
   return (
-    <div className={s.overview} >
+    <div className={s.overview} onMouseLeave={() => isHome && setTitle(null)} >
       <h1 className={cn(!isHome && s.active)}>{title}</h1>
       <div className={s.leftColumn}>
         {overview.leftColumn.map((block, index) =>
