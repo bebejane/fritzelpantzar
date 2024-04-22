9a3792c0f559b6dda2a92efc2e5a5a4d0b1dc0ef
Bebe Jane <bjornthief@gmail.com>
2024-04-22 18:55:26 +0200

    loading

--- components/blocks/SelectedProjectBlock.tsx
+++ components/blocks/SelectedProjectBlock.tsx
@@ -11,15 +11,14 @@ export type LayoutProps = { data: SelectedProjectBlockRecord }
 export default function SelectedProjectBlock({ data: { image, project } }: LayoutProps) {
 
 	return (
-		<section className={s.selectedProject}>
-			<Link href={`/projects/${project.slug}`} scroll={false} prefetch={true}>
-				<Image
-					data={image.responsiveImage}
-					fadeInDuration={0}
-					usePlaceholder={false}
-					intersectionMargin="0px 0px 2000px 0px"
-				/>
-			</Link>
-		</section>
+		<Link href={`/projects/${project.slug}`} scroll={false} prefetch={true} className={s.project}>
+			<Image
+				data={image.responsiveImage}
+				fadeInDuration={0}
+				usePlaceholder={false}
+				priority={true}
+				intersectionMargin="0px 0px 2000px 0px"
+			/>
+		</Link>
 	)
 }
