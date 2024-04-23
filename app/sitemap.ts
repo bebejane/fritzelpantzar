ef8efd383654d4139af5fa1d8c7f44939da0ad5d
Bebe Jane <bjornthief@gmail.com>
2024-04-23 14:04:06 +0200

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,4 +1,6 @@
 import { MetadataRoute } from 'next'
+import { apiQuery } from 'next-dato-utils/api';
+import { AllProjectsDocument } from '../graphql';
 
 const staticRoutes: MetadataRoute.Sitemap = [
   {
@@ -6,12 +8,25 @@ const staticRoutes: MetadataRoute.Sitemap = [
     lastModified: new Date(),
     changeFrequency: 'daily',
     priority: 1,
+  },
+  {
+    url: `${process.env.NEXT_PUBLIC_SITE_URL}/about`,
+    lastModified: new Date(),
+    changeFrequency: 'monthly',
+    priority: 1,
   }
 ]
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const { allProjects } = await apiQuery<AllProjectsQuery, AllProjectsQueryVariables>(AllProjectsDocument, {
+    all: true,
+    tags: ['project', 'about']
+  });
 
-  return [
-    ...staticRoutes
-  ]
+  return staticRoutes.concat(allProjects.map(({ slug }) => ({
+    url: `${process.env.NEXT_PUBLIC_SITE_URL}/projects/${slug}`,
+    lastModified: new Date(),
+    changeFrequency: 'weekly',
+    priority: 1,
+  })))
 }
