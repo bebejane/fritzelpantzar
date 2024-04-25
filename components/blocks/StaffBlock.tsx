0793271f1ab156ed009bc654ff0c0fd7e1b2c2c3
konst-teknik <information@konst-teknik.se>
2024-04-25 12:25:03 +0200

    Fix

--- components/blocks/StaffBlock.tsx
+++ components/blocks/StaffBlock.tsx
@@ -11,11 +11,13 @@ export default function StaffBlock({ data: { people } }: LayoutProps) {
 		<ul className={s.staff}>
 			{people.map(({ id, image, firstName, lastName, phone, email, title }) =>
 				<li key={id}>
-					<Image data={image?.responsiveImage} />
-					{firstName} {lastName}
-					{title && <span>{title}</span>}
-					{phone && <a href={`tel:${phone}`}>{phone}</a>}
-					{email && <a href={`mailto:${email}`}>{email}</a>}
+					<figure>
+						<Image data={image?.responsiveImage} />
+					</figure>
+					{firstName} {lastName}<br />
+					{title && <span>{title}</span>}<br />
+					{phone && <a href={`tel:${phone}`}>{phone}</a>}<br />
+					{email && <a href={`mailto:${email}`}>{email}</a>}<br />
 				</li>
 			)}
 		</ul>
