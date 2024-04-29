4e733eca99169e5b55d5f27c0a6c083bc46e8dde
Bebe Jane <bjornthief@gmail.com>
2024-04-29 15:35:57 +0200

    ja

--- components/blocks/index.ts
+++ components/blocks/index.ts
@@ -1,3 +1,2 @@
 export { default as MediaRowBlock } from './MediaRowBlock';
-export { default as SelectedProjectBlock } from './SelectedProjectBlock';
 export { default as StaffBlock } from './StaffBlock';
