48f205491a2332b8fc3975fee843c0ffd2517bba
Bebe Jane <bjornthief@gmail.com>
2024-05-15 12:43:13 +0200

    fix video

--- components/blocks/MediaRowBlock.tsx
+++ components/blocks/MediaRowBlock.tsx
@@ -3,20 +3,25 @@
 import s from './MediaRowBlock.module.scss'
 import cn from 'classnames'
 import { Image } from 'react-datocms'
+import { VideoPlayer } from 'next-dato-utils/components'
 
 export type LayoutProps = { data: MediaRowBlockRecord }
 
 export default function MediaRowBlock({ data: { mediaAsset } }: LayoutProps) {
-
+	console.log(mediaAsset)
 	return (
 		<section className={s.mediaRow}>
 			{mediaAsset.map((asset, i) => (
-				<figure key={i} className={mediaAsset.length > 1 ? s.multi : s.single}>
-					<Image
-						data={asset.responsiveImage}
-						intersectionMargin="0px 0px 200% 0px"
-					/>
-				</figure>
+				asset.responsiveImage ?
+					<figure key={i} className={mediaAsset.length > 1 ? s.multi : s.single}>
+						<Image
+							data={asset.responsiveImage}
+							intersectionMargin="0px 0px 200% 0px"
+						/>
+					</figure>
+					: asset.video ?
+						<VideoPlayer key={i} data={asset as FileField} className={mediaAsset.length > 1 ? s.multi : s.single} />
+						: null
 			))}
 		</section>
 	)
