4bcb46a864fe28011b81c2e15a38f5e2898b119c
Bebe Jane <bjornthief@gmail.com>
2025-03-12 13:30:13 +0100

    ddd

--- components/blocks/MediaRowBlock.tsx
+++ components/blocks/MediaRowBlock.tsx
@@ -1,32 +1,28 @@
-'use client'
+'use client';
 
-import s from './MediaRowBlock.module.scss'
-import cn from 'classnames'
-import { Image } from 'react-datocms'
-import { VideoPlayer } from 'next-dato-utils/components'
+import s from './MediaRowBlock.module.scss';
+import cn from 'classnames';
+import { Image } from 'react-datocms';
+import { VideoPlayer } from 'next-dato-utils/components';
 
-export type LayoutProps = { data: MediaRowBlockRecord }
+export type LayoutProps = { data: MediaRowBlockRecord };
 
 export default function MediaRowBlock({ data: { mediaAsset } }: LayoutProps) {
-
 	return (
 		<section className={s.mediaRow}>
-			{mediaAsset.map((asset, i) => (
-				asset.responsiveImage ?
+			{mediaAsset.map((asset, i) =>
+				asset.responsiveImage ? (
 					<figure key={i} className={mediaAsset.length > 1 ? s.multi : s.single}>
-						<Image
-							data={asset.responsiveImage}
-							intersectionMargin="0px 0px 200% 0px"
-						/>
+						<Image data={asset.responsiveImage} intersectionMargin='0px 0px 2000px 0px' />
 					</figure>
-					: asset.video ?
-						<VideoPlayer
-							key={i}
-							data={asset as FileField}
-							className={mediaAsset.length > 1 ? s.multi : s.single}
-						/>
-						: null
-			))}
+				) : asset.video ? (
+					<VideoPlayer
+						key={i}
+						data={asset as FileField}
+						className={mediaAsset.length > 1 ? s.multi : s.single}
+					/>
+				) : null
+			)}
 		</section>
-	)
+	);
 }
